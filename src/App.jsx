7292ae34d8bd6071f62a9a1a1acf508ebaa8270b
DelwinPrakash <delwinprakash499@gmail.com>
2025-01-11 23:02:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+            <h1 className="bg-black text-yellow-300 text-4xl">App</h1>
+        </div>
+    );
+}
