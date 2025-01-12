d8e46685fc2e17040822be1c5fe446c049f5be23
DelwinPrakash <delwinprakash499@gmail.com>
2025-01-13 01:13:51 +0530

    nav fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { GoHome } from "react-icons/go";
 import { RiRobot3Line } from "react-icons/ri";
 import { FaRegUserCircle } from "react-icons/fa";
 
 export default function Nav() {
+
+    const loc = useLocation();
+
     return (
         <nav>
-            <div className="sm:hidden fixed bottom-0 left-0 w-full bg-zinc-800 text-white h-14 max-h-14 flex justify-around items-center">
-                <Link to={"/"} className="flex flex-col items-center">
+            <div className="fixed bg-zinc-800 text-white flex sm:flex-col justify-around items-center w-full sm:w-16 h-14 sm:h-full bottom-0 sm:bottom-auto sm:left-0">
+                <Link to={"/"} className={`flex flex-col items-center ${loc.pathname === "/" ? "text-white" : "text-gray-500"}`}>
                     <GoHome size={25} />
-                    <span>Home</span>
+                    <span className="text-sm sm:text-xs">Home</span>
                 </Link>
-                <Link to={"/ai"} className="flex flex-col items-center">
+                <Link to={"/ai"} className={`flex flex-col items-center ${loc.pathname === "/ai" ? "text-white" : "text-gray-500"}`}>
                     <RiRobot3Line size={25} />
-                    <span>Cleanse</span>
+                    <span className="text-sm sm:text-xs">Cleanse</span>
                 </Link>
-                <Link to={"/profile"} className="flex flex-col items-center">
+                <Link to={"/profile"} className={`flex flex-col items-center ${loc.pathname === "/profile" ? "text-white" : "text-gray-500"}`}>
                     <FaRegUserCircle size={25} />
-                    <span>Profile</span>
-                </Link>
-            </div>
-            <div className="hidden sm:flex fixed bg-zinc-800 text-white h-full w-16 max-w-16 flex-col justify-around items-center">
-                <Link to={"/"} className="flex flex-col items-center">
-                    <GoHome size={25} />
-                    <span className="text-sm">Home</span>
-                </Link>
-                <Link to={"/ai"} className="flex flex-col items-center">
-                    <RiRobot3Line size={25} />
-                    <span className="text-sm">Cleanse</span>
-                </Link>
-                <Link to={"/profile"} className="flex flex-col items-center">
-                    <FaRegUserCircle size={25} />
-                    <span className="text-sm mt-1">Profile</span>
+                    <span className="text-sm sm:text-xs sm:mt-1">Profile</span>
                 </Link>
             </div>
         </nav>
