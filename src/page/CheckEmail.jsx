fcc4827c5ea065b92bbf0c12de378220b61d4901
= <delwinprakash499@gmail.com>
2025-03-15 22:28:02 +0530

    added new pages

---
+++ src/page/CheckEmail.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function CheckEmail(){
+    const navigate = useNavigate();
+    return (
+        <div className="flex items-center justify-center w-full h-screen bg-zinc-950 z-0">
+            <div className="max-w-md w-full md:bg-zinc-900 shadow-lg rounded-lg p-8">
+                <h1 className="text-3xl font-bold text-center text-gray-100 mb-4">
+                    Verify Your Email
+                </h1>
+                <p className="text-lg text-center text-gray-300 mb-6">
+                    {/* Please check your email inbox for a verification link. */}
+                    We've sent a verification link to your email address
+                </p>
+                <p className="text-lg text-center text-gray-300 mb-6">
+                    You might need to <span className="font-bold text-gray-200">check your spam folder</span>
+                </p>
+                <div className="text-center">
+                    {/* <button
+                        className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300"
+                    >
+                        Resend Verification Email
+                    </button> */}
+                    <p className="mt-4 text-sm text-gray-500">
+                            Already verified? <span className="font-medium text-blue-500 cursor-pointer" onClick={() => navigate("/login")}>Go to login</span>.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
---
+++ src/page/VerifyEmail.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function VerifyEmail() {
+    const [message, setMessage] = useState("Verifying your email...");
+    const [searchParams] = useSearchParams();
+    const token = searchParams.get("token");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(token){
+            console.log("verifyEmail page", token)
+            axios.get(`http://localhost:3000/api/verify-email?token=${token}`).then((response) => {
+                setMessage(response.data.message);
+            }).catch((error) => {
+                setMessage(error.response?.data?.message || "Something went wrong!"); 
+            });
+        }else{
+            setMessage("Invalid verification link!");
+        }
+        setTimeout(() => {
+            navigate("/login");
+        }, 5000);
+    }, [token, navigate]);
+
+    return (
+        <div className="w-full h-screen bg-zinc-950 z-0">
+            <div className="flex flex-col items-center justify-center h-full">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+                <p className="mt-3 text-xl text-white">{message}</p>
+            </div>
+        </div>
+    )
+}
