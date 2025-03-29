5ba1086dc7a23d1d3cd9fe459e1ccf6c1905dc12
= <delwinprakash499@gmail.com>
2025-03-30 01:21:36 +0530

    user-details schema

--- backend/models/UserDetails.js
+++ backend/models/UserDetails.js
@@ -2,11 +2,77 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const userDetailSchema = new Schema({
-    
+    userID:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    pickupAddress: {
+        type: String,
+        required: true,
+    },
+    street: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    zipCode: {
+        type: String,
+        required: true,
+    },
+    preferredDate: {
+        type: String,
+        required: true,
+    },
+    preferredTime: {
+        type: String
+    },
+    eWasteType: {
+        type: [String],
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    condition: {
+        type: String,
+        required: true
+    },
+    specialHandling: {
+        type: String,
+        required: true
+    },
+    pickupDescription: {
+        type: String,
+        required: true
+    },
+    alternateContactNumber: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ["pending", "accepted", "rejected"]
+    },
     createdAt: {
         type: Date,
         default: Date.now
     }
 })
 
-export default mongoose.model("User", userDetailSchema)
+export default mongoose.model("UserDetails", userDetailSchema)
