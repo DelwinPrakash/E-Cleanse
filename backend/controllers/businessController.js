c4b0fcc2c3a8716bda145c7a7e06702b34b84249
= <delwinprakash499@gmail.com>
2025-03-31 13:24:45 +0530

    recycle item controller

--- backend/controllers/businessController.js
+++ backend/controllers/businessController.js
@@ -1,7 +1,10 @@
+import mongoose from "mongoose";
 import BusinessDetails from "../models/BusinessDetails.js";
 import RecycleItem from "../models/RecycleItem.js";
 import User from "../models/User.js";
 import UserDetails from "../models/UserDetails.js";
+
+const { ObjectId } = mongoose.Types;
 
 const completeBusinessProfile = async (req, res) => {
     const { businessDetail, userID } = req.body;
@@ -40,7 +43,7 @@ const getBusinessDetails = async (req, res) => {
         const businessDetails = await BusinessDetails.findOne({ userID }).exec();
         
         if(!businessDetails){
-            return res.status(404).json({message: "User details not found!"});
+            return res.status(404).json({message: "Business details not found!"});
         }
         
         res.status(200).json({
@@ -85,4 +88,92 @@ const recycleItem = async (req, res) => {
     }
 }
 
-export { completeBusinessProfile, getBusinessDetails, updateUserStatus, recycleItem };
+const getBusinessProfile = async (req, res) => {
+    const businessID = new ObjectId(req.params.userID);
+
+    try{
+        
+        // if(!recycleDetails){
+            //     return res.status(404).json({message: "Recycle details not found!"});
+            // }
+            
+        // const businessDetails = await BusinessDetails.findOne({ userID: businessID }).exec();
+        // const recycleDetails = await RecycleItem.find({ businessID }).exec();
+        // if(recycleDetails){
+        //     userDetails = await UserDetails.findOne({ _id: recycleDetails[0].userID }).exec();
+        // }
+        
+        // res.status(200).json({
+        //     success: true,
+        //     recycleDetails: recycleDetails || null,
+        //     businessDetails,
+        //     userDetails
+        // });
+        
+        const recycleDetails = await RecycleItem.aggregate([
+            
+            {
+                $lookup: {
+                    from: "userdetails",
+                    localField: "userID",
+                    foreignField: "_id",
+                    as: "userDetails"
+                }
+            },
+            {
+                $unwind: "$userDetails"          
+            },
+            {
+                $lookup: {
+                    from: "users",              
+                    localField: "userDetails.userID",
+                    foreignField: "_id",       
+                    as: "userInfo"
+                }
+            },
+            {
+                $unwind: "$userInfo"          
+            },
+            {
+                $match: {
+                    businessID: businessID
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    userID: 1,
+                    businessID: 1,
+                    status: 1,
+                    "userDetails.fullName": 1,
+                    "userDetails.eWasteType": 1,
+                    "userDetails.phoneNumber": 1,
+                    "userDetails.pickupAddress": 1,
+                    "userInfo.email": 1
+                }
+            },
+        ]);
+
+        if(recycleDetails.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "No recycle details found for the specified business!",
+                recycleDetails: null
+            });
+        }
+        
+        res.status(200).json({
+            success: true,
+            recycleDetails
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch all details!",
+            error: error.message
+        })
+    }
+}
+
+export { completeBusinessProfile, getBusinessDetails, updateUserStatus, recycleItem, getBusinessProfile };
