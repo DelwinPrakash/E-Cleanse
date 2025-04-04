1816c4aaaaac942a63d9674da58ae1e8f42da978
= <delwinprakash499@gmail.com>
2025-04-04 21:34:13 +0530

    api fix

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import handleNewuser from "./backend/controllers/registerController.js";
 import handleLogin from "./backend/controllers/authController.js";
 import verifyJWT from "./backend/controllers/verifyJWT.js";
 import verifyEmail from "./backend/controllers/verifyEmail.js";
-import { completeBusinessProfile, getBusinessDetails, getBusinessProfile, recycleItem, updateUserStatus } from "./backend/controllers/businessController.js";
-import { RecycleWaste, getAllUsers, getUserProfile } from "./backend/controllers/userController.js";
+import { collectEWaste, completeBusinessProfile, getBusinessDetails, getBusinessProfile, getRecycleHistory, recycleItem, updateUserStatus } from "./backend/controllers/businessController.js";
+import { RecycleWaste, getAllUsers, getUserProfile, getUserRecycleHistory } from "./backend/controllers/userController.js";
 
 const app = express();
 
@@ -45,6 +45,12 @@ app.post("/api/recycle-item", recycleItem);
 app.get("/api/business-profile/:userID", getBusinessProfile);
 
 app.get("/api/user-profile/:userID", getUserProfile);
+
+app.post("/api/collect-waste", collectEWaste);
+
+app.get("/api/recent-order/:userID", getRecycleHistory);
+
+app.get("/api/user-recent-order/:userID", getUserRecycleHistory);
 
 // app.get("/api/user-profile/pending/:userID", getUserPendingItems);
 
